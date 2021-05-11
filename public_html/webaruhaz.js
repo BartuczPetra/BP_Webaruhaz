57e41814ab5034e84d8ac7ed85b3f8fa9d6bd4a3
BartuczPetra <bartucz.petra@gmail.com>
2021-05-11 17:53:55 +0200

    3. feladat

---
+++ public_html/webaruhaz.js
@@ -0,0 +1,6 @@
+$(function(){
+    
+});
+
+var termekJSON='[{"id":"a1","nev":"Tej","ar":"500 Ft","tipus":"laktózmentes","szin":"lila"},{"id":"c4","nev":"Nadrág","ar":"2000 Ft","tipus":"farmer","szin":"kék"}]';
+var termekObjektum=JSON.parse(termekJSON);
